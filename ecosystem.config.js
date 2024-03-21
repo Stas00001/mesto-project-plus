1f456e6a531ff3951e445dfbfa152880c3a01234
Stas00001 <116278430+Stas00001@users.noreply.github.com>
2024-03-21 22:09:30 +0300

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,7 +21,7 @@ module.exports = {
       repo: 'https://github.com/Stas00001/mesto-project-plus.git',
       path: DEPLOY_PATH,
       // 'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build',
+      'post-deploy': 'npm i && npm run start',
     },
   },
 };
